5fdc058e1b796d18bfec0039f9863ed98a0b23da
Danilo Carlos Marques <marquesdanilocarlos@gmail.com>
2025-02-27 17:06:38 -0300

    Aula 06 - Definição do status code no cabeçalho da resposta

--- api/src/server.js
+++ api/src/server.js
@@ -2,7 +2,8 @@ import http from 'node:http';
 
 const server = http.createServer((req, res) => {
     const {method} = req;
-    return res.end(`Método: ${method}`);
+
+    return res.writeHead(201).end(`Método: ${method}`);
 });
 
 server.listen(3333);

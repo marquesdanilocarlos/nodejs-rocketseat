5c81fe709d5a52db05e2bafdc9ca0bd41f6851df
Danilo Carlos Marques <marquesdanilocarlos@gmail.com>
2025-02-28 20:42:06 -0300

    Aula 17 - Definição de métodos privados

--- api/src/database.js
+++ api/src/database.js
@@ -3,37 +3,34 @@ import * as fs from 'node:fs/promises';
 const DATABASE_PATH = new URL('db.json', import.meta.url);
 
 export class Database {
-    database = {};
+    #database = {};
 
     constructor() {
 
         fs.readFile(DATABASE_PATH, 'utf8').then((data) => {
-            this.database = JSON.parse(data);
+            this.#database = JSON.parse(data);
         }).catch(() => {
             console.log('Não foi possível recuperar os dados do arquivo.')
         });
 
-        this.persist();
+        this.#persist();
     }
 
-    persist() {
-        fs.writeFile(DATABASE_PATH, JSON.stringify(this.database));
+    #persist() {
+        fs.writeFile(DATABASE_PATH, JSON.stringify(this.#database));
     }
 
     insert(table, data) {
-        if (!Array.isArray(this.database[table])) {
-            this.database[table] = [data];
+        if (!Array.isArray(this.#database[table])) {
+            this.#database[table] = [data];
             return;
         }
 
-        this.database[table].push(data);
-        this.persist();
+        this.#database[table].push(data);
+        this.#persist();
     }
 
     select(table) {
-
-
-
-        return this.database[table];
+        return this.#database[table] ?? [];
     }
 }

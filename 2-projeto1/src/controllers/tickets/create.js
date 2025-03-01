46abaf4ffa4c62b81fd4fe5fea18bba0b840475f
Danilo Carlos Marques <marquesdanilocarlos@gmail.com>
2025-03-01 00:32:15 -0300

    Aula 23 - Criação do objeto de ticket

--- 2-projeto1/src/controllers/tickets/create.js
+++ 2-projeto1/src/controllers/tickets/create.js
@@ -1,3 +1,17 @@
+import {randomUUID} from 'node:crypto';
 export default function create({request, response}) {
-    return response.end('Rota de criação de tickets');
+
+    const {equipament, description, user_name} = request.body;
+
+    const ticket = {
+        id: randomUUID(),
+        equipament,
+        description,
+        user_name,
+        status: 'open',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+    }
+
+    return response.end(JSON.stringify(ticket));
 }

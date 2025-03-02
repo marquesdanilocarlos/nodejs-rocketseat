4fd28952f26136cc821624e71d0d91613754efb7
Danilo Carlos Marques <marquesdanilocarlos@gmail.com>
2025-03-02 12:42:28 -0300

    Aula 40 - Obtenção de dados do corpo da requisição

--- 3-api-rest/src/server.ts
+++ 3-api-rest/src/server.ts
@@ -2,6 +2,7 @@ import express, {Request, Response} from 'express';
 
 const PORT: number = 3333;
 const app = express();
+app.use(express.json());
 
 
 app.get("/products", (request: Request, response: Response) => {
@@ -9,5 +10,10 @@ app.get("/products", (request: Request, response: Response) => {
 
     response.send(`Página ${page} com limit ${limit}`)
 });
+
+app.post("/products", (request: Request, response: Response) => {
+    const {name, price} = request.body;
+    response.send(`Produto ${name} custa ${price}`);
+})
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));

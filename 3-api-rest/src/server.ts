1b072c33cc8d458fcb93c542f519b43e0a7f4a09
Danilo Carlos Marques <marquesdanilocarlos@gmail.com>
2025-03-02 01:12:02 -0300

    Aula 39 - Obtenção de parâmetros da querystring

--- 3-api-rest/src/server.ts
+++ 3-api-rest/src/server.ts
@@ -3,15 +3,11 @@ import express, {Request, Response} from 'express';
 const PORT: number = 3333;
 const app = express();
 
-interface Params {
-    id: number;
-    user: string;
-}
 
+app.get("/products", (request: Request, response: Response) => {
+    const {page, limit} = request.query;
 
-app.get("/products/:id/:user", (request: Request<Params>, response: Response) => {
-    const {id, user} = request.params;
-    response.send(typeof id);
+    response.send(`Página ${page} com limit ${limit}`)
 });
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));

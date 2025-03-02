a7397357e43239b24aeae360a7b9fad416b66cce
Danilo Carlos Marques <marquesdanilocarlos@gmail.com>
2025-03-02 00:51:49 -0300

    Aula 37 - Primeira rota com Express

--- 3-api-rest/src/server.ts
+++ 3-api-rest/src/server.ts
@@ -1,6 +1,10 @@
 import express from 'express';
 
-const PORT = 3333;
+const PORT: number = 3333;
 const app = express();
+
+app.get("/", (request, response) => {
+    response.send("Hello World Express!");
+});
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));

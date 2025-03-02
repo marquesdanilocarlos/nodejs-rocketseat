05755a32d715a2ed82553cbf86ae7fa1d274b5ba
Danilo Carlos Marques <marquesdanilocarlos@gmail.com>
2025-03-02 01:07:19 -0300

    Aula 38 - Obtenção de parâmetros da rota

--- 3-api-rest/src/server.ts
+++ 3-api-rest/src/server.ts
@@ -1,10 +1,17 @@
-import express from 'express';
+import express, {Request, Response} from 'express';
 
 const PORT: number = 3333;
 const app = express();
 
-app.get("/", (request, response) => {
-    response.send("Hello World Express!");
+interface Params {
+    id: number;
+    user: string;
+}
+
+
+app.get("/products/:id/:user", (request: Request<Params>, response: Response) => {
+    const {id, user} = request.params;
+    response.send(typeof id);
 });
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));

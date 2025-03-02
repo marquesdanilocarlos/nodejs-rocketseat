ef33a337c3758646f9c2d54bb40964b1b1345a78
Danilo Carlos Marques <marquesdanilocarlos@gmail.com>
2025-03-02 00:47:59 -0300

    Aula 36 - Instalação do Express e criação do server

--- 3-api-rest/src/server.ts
+++ 3-api-rest/src/server.ts
@@ -1,6 +1,6 @@
-function sum(a: number, b: number): number {
-  return a + b;
-}
+import express from 'express';
 
-const result: number = sum(10, 78);
-console.log(`Resultado da soma: ${result}`);
+const PORT = 3333;
+const app = express();
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));

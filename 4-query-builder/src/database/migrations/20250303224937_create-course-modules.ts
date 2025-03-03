8af948a0a9bea0248eda808a3733b4254a12bf56
Danilo Carlos Marques <marquesdanilocarlos@gmail.com>
2025-03-03 19:53:19 -0300

    Aula 66 - Criação de nova tabela e relacionamento com Knex

---
+++ 4-query-builder/src/database/migrations/20250303224937_create-course-modules.ts
@@ -0,0 +1,18 @@
+import type {Knex} from "knex";
+
+
+export async function up(knex: Knex): Promise<void> {
+
+    await knex.schema.createTable('course_modules', table => {
+        table.increments('id').primary();
+        table.text('name').notNullable();
+
+        table.integer('course_id').references('id').inTable('courses').unsigned().notNullable();
+    });
+}
+
+
+export async function down(knex: Knex): Promise<void> {
+    await knex.schema.dropTable('course_modules');
+}
+

bba7a36b8502bdd9b21f66b108e190845cec9a60
Danilo Carlos Marques <marquesdanilocarlos@gmail.com>
2025-03-03 20:23:36 -0300

    Aula 67 - Cadastro de módulos com relacionamento com Knex

--- 4-query-builder/knexfile.ts
+++ 4-query-builder/knexfile.ts
@@ -3,6 +3,11 @@ export default {
     connection: {
         filename: './src/database/database.db'
     },
+    pool: {
+        afterCreate: (conn: any, done:any) => {
+            conn.run('PRAGMA foreign_keys = ON', done);
+        }
+    },
     useNullAsDefault: true,
     migrations: {
         extension: 'ts',
--- 4-query-builder/src/index.ts
+++ 4-query-builder/src/index.ts
@@ -16,7 +16,7 @@ app.post('/courses', async (request: Request, response: Response): Promise<void>
 
 
 app.get('/courses', async (request: Request, response: Response): Promise<void> => {
-    const courses = await knex('courses')
+    const courses: any[] = await knex('courses')
         .select()
         .orderBy('created_at', 'desc');
 
@@ -39,6 +39,22 @@ app.delete('/courses/:id', async (request: Request, response: Response): Promise
     await knex('courses').where({id}).del();
 
     response.status(200).json();
-})
+});
+
+app.post('/modules', async (request: Request, response: Response): Promise<void> => {
+    const {name, course_id} = request.body;
+
+    await knex('course_modules').insert({
+        name,
+        course_id
+    });
+
+    response.status(201).json();
+});
+
+app.get('/modules', async (request: Request, response: Response): Promise<void> => {
+    const modules: any[] = await knex('course_modules').select();
+    response.status(200).json(modules);
+});
 
 app.listen(3333);

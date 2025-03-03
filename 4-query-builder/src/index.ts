f5c4f9eb45459f68edd18ab91bc9882216f03211
Danilo Carlos Marques <marquesdanilocarlos@gmail.com>
2025-03-03 16:09:52 -0300

    Aula 63 - Update de dados com Knex

--- 4-query-builder/src/index.ts
+++ 4-query-builder/src/index.ts
@@ -4,23 +4,32 @@ import knex from "./database/knex"
 const app = express();
 app.use(express.json());
 
-app.post('/courses', async(request: Request, response: Response):Promise<void> => {
+app.post('/courses', async (request: Request, response: Response): Promise<void> => {
     const {name} = request.body;
 
     await knex('courses').insert({
-       name
+        name
     });
 
     response.status(201).json({name});
 });
 
 
-app.get('/courses', async(request: Request, response: Response):Promise<void> => {
-   const courses = await knex('courses')
-       .select()
-       .orderBy('created_at', 'desc');
+app.get('/courses', async (request: Request, response: Response): Promise<void> => {
+    const courses = await knex('courses')
+        .select()
+        .orderBy('created_at', 'desc');
 
-   response.json(courses);
+    response.json(courses);
+});
+
+app.put('/courses/:id', async (request: Request, response: Response): Promise<void> => {
+    const {id} = request.params;
+    const {name} = request.body;
+
+    await knex('courses').update({name}).where({id});
+
+    response.status(200).json();
 });
 
 app.listen(3333);

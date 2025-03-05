c34d796b544a5a0525b6ebfb50bc2aad89f53f07
Danilo Carlos Marques <marquesdanilocarlos@gmail.com>
2025-03-05 20:26:05 -0300

    Aula 75 - Instalação do Zod e tratamento de erros de validação no middleware global de erros

--- 5-api-restaurant/src/middlewares/ErrorHandler.ts
+++ 5-api-restaurant/src/middlewares/ErrorHandler.ts
@@ -1,10 +1,15 @@
 import {Request, Response, NextFunction} from 'express';
-import AppError from "@/errors/AppError";
+import AppError from '@/errors/AppError';
+import {ZodError} from 'zod';
 
-export default function errorHandler(error: Error, req: Request, res: Response, next: NextFunction):Response {
+export default function errorHandler(error: Error, req: Request, res: Response, next: NextFunction): Response {
 
     if (error instanceof AppError) {
         return res.status(error.statusCode).json({message: error.message});
+    }
+
+    if (error instanceof ZodError) {
+        return res.status(400).json({message: 'Erro de validação', issues: error.format()});
     }
 
     return res.status(500).json({message: 'Erro interno do servidor.'});

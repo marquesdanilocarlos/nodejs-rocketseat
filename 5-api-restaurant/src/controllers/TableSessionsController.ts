fc57678f087233e048aec5bc7a28d7a05a08023e
Danilo Carlos Marques <marquesdanilocarlos@gmail.com>
2025-03-06 23:09:13 -0300

    Aula 87 - Fechamento de mesa

--- 5-api-restaurant/src/controllers/TableSessionsController.ts
+++ 5-api-restaurant/src/controllers/TableSessionsController.ts
@@ -1,6 +1,6 @@
 import {Request, Response, NextFunction} from 'express';
 import {CatchError} from "@/decorators/CatchError";
-import bodySchema from "@/validators/tableSessionValidator";
+import bodySchema, {idValidator} from "@/validators/tableSessionValidator";
 import knexInstance from "@/database/knex";
 import AppError from "@/errors/AppError";
 
@@ -24,5 +24,30 @@ export default class TableSessionsController {
         await knexInstance<TableSessionType>('tables_sessions').insert({table_id, opened_at: knexInstance.fn.now()});
 
         return res.status(201).json();
+    }
+
+    @CatchError()
+    async index(req: Request, res: Response, next: NextFunction): Promise<any> {
+        const sessions: TableSessionType[] = await knexInstance<TableSessionType>('tables_sessions')
+            .orderBy('closed_at');
+
+        return res.json({sessions});
+    }
+
+    @CatchError()
+    async close(req: Request, res: Response, next: NextFunction): Promise<any> {
+        const id = idValidator.parse(req.params.id);
+        const session = await knexInstance<TableSessionType>('tables_sessions')
+            .where('id', id)
+            .whereNull('closed_at')
+            .first();
+
+        if (!session) {
+            throw new AppError('Sessão da mesa não encontrada.');
+        }
+
+        await knexInstance<TableSessionType>('tables_sessions').update({closed_at: knexInstance.fn.now()}).where('id', id);
+
+        res.json();
     }
 }
--- 5-api-restaurant/src/routes/table-sessions.routes.ts
+++ 5-api-restaurant/src/routes/table-sessions.routes.ts
@@ -5,5 +5,7 @@ const tableSessionRouter: Router = Router();
 const tableSessionController: TableSessionsController = new TableSessionsController();
 
 tableSessionRouter.post('/', tableSessionController.create);
+tableSessionRouter.get('/', tableSessionController.index);
+tableSessionRouter.patch('/:id', tableSessionController.close);
 
 export default tableSessionRouter;

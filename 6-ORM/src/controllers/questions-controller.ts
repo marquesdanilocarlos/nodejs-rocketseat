3d7d55ba2fb2a77a12013b64e63437a4c429c873
Danilo Carlos Marques <marquesdanilocarlos@gmail.com>
2025-03-08 19:45:31 -0300

    Aula 104 - Remoção de pergunta

--- 6-ORM/src/controllers/questions-controller.ts
+++ 6-ORM/src/controllers/questions-controller.ts
@@ -39,6 +39,10 @@ class QuestionsController {
     }
 
     async remove(request: Request, response: Response) {
+        const {id} = request.params;
+
+        await prisma.question.delete({where: {id}});
+
         return response.json();
     }
 }

cb43c74d6f0f60aa1dd72f0bc37888184a52a3fc
Danilo Carlos Marques <marquesdanilocarlos@gmail.com>
2025-03-10 21:39:55 -0300

    Aula 109 - Criação do Token de autenticação

--- 7-jwt/src/config/auth.ts
+++ 7-jwt/src/config/auth.ts
@@ -1,6 +1,8 @@
-export default authConfig = {
+const authConfig = {
     jwt: {
         secret: process.env.AUTH_SECRET || 'default',
         expiresIn: process.env.AUTH_EXPIRES,
     }
 }
+
+export default authConfig;
--- 7-jwt/src/controllers/SessionsController.ts
+++ 7-jwt/src/controllers/SessionsController.ts
@@ -1,7 +1,29 @@
 import {Request, Response} from "express";
+import AppError from "@/error/AppError";
+import authConfig from "@/config/auth";
+import {sign} from "jsonwebtoken"
+
 export default class SessionsController {
 
     async create(request: Request, response: Response): Promise<any> {
-        return response.status(201).json();
+        const {username, password} = request.body;
+
+        const fakeUser = {
+            id: 1,
+            username: "danilo",
+            password: "123456"
+
+        };
+
+        if (username !== fakeUser.username || password !== fakeUser.password) {
+            throw new AppError("Usuário ou senha incorretos.", 401);
+        }
+        const {secret} = authConfig.jwt;
+        const token = sign({}, secret, {
+            expiresIn: authConfig.jwt.expiresIn,
+            subject: String(fakeUser.id)
+        });
+
+        return response.status(201).json(token);
     }
 }

ab9326fd39c127ba705d16be75ff8771c7f6399a
Danilo Carlos Marques <marquesdanilocarlos@gmail.com>
2025-03-13 02:19:50 -0300

    Aula 116 - Agrupamento de teste "describe"

--- 8-testes/src/sum.test.ts
+++ 8-testes/src/sum.test.ts
@@ -1,7 +1,15 @@
 import {sum} from "./index";
 
-test('sum must be 10', () => {
-    const result = sum(3, 7);
+describe('sum', () => {
+    test('sum must be 10', () => {
+        const result = sum(3, 7);
 
-    expect(result).toBe(11);
+        expect(result).toBe(10);
+    });
+
+    test('sum must be 4', () => {
+        const result = sum(2, 2);
+
+        expect(result).toBe(4);
+    });
 });

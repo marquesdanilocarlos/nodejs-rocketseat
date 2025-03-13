9000eb3f17c2b63b14299ff192ed3816886d7233
Danilo Carlos Marques <marquesdanilocarlos@gmail.com>
2025-03-12 23:35:28 -0300

    Aula 115 - Escrita do primeiro teste

--- 8-testes/src/subtraction.test.ts
+++
@@ -1,3 +0,0 @@
-test('subtraction', () => {
-    console.log("Teste SUBTRACTION OK!");
-});
--- 8-testes/src/sum.test.ts
+++ 8-testes/src/sum.test.ts
@@ -1,3 +1,7 @@
-test('sum', () => {
-    console.log("Teste SUM OK!");
+import {sum} from "./index";
+
+test('sum must be 10', () => {
+    const result = sum(3, 7);
+
+    expect(result).toBe(11);
 });

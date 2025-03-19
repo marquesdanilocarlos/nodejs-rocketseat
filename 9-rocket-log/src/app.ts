076a72b6014e195fba552c478e70cf0046ee5356
Danilo Carlos Marques <marquesdanilocarlos@gmail.com>
2025-03-18 21:59:31 -0300

    Aula 123 - Middleware de tratamento de erros

--- 9-rocket-log/src/app.ts
+++ 9-rocket-log/src/app.ts
@@ -1,6 +1,11 @@
+import "express-async-errors";
 import express from "express";
+import AppError from "@/error/AppError";
 
 const app = express();
 app.use(express.json());
+/*@ts-ignore*/
+app.use(AppError.errorHandler)
+
 
 export default app;

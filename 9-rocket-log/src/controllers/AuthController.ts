52a938a0a549ff4c5601c111e8592e79b4147537
Danilo Carlos Marques <marquesdanilocarlos@gmail.com>
2025-04-11 22:57:18 -0300

    Aula 132 - Recuperação do usuário

--- 9-rocket-log/src/controllers/AuthController.ts
+++ 9-rocket-log/src/controllers/AuthController.ts
@@ -1,7 +1,27 @@
-import {Request} from "express";
+import {Request, Response} from "express";
+import prisma from "@/database/prisma";
+import authValidator from "@/validators/authValidator";
+import AppError from "@/error/AppError";
+import bcrypt from "bcryptjs";
 
 export default class AuthController {
-    async login(request: Request, response: Response): Promise {
-        return response.json({message: 'Login'});
+    async login(request: Request, response: Response): Promise<any> {
+        const {email, password} = authValidator.parse(request.body);
+        const user = await prisma.user.findFirst({
+            where: {email}
+        });
+        const errorMessage: string = 'Email ou senhas incorretos.';
+
+        if (!user) {
+            throw new AppError(errorMessage, 401);
+        }
+
+        const passwordMatch = await bcrypt.compare(password, user.password);
+
+        if (!passwordMatch) {
+            throw new AppError(errorMessage, 401);
+        }
+
+        return response.json({message: 'Autenticado com sucesso.'});
     }
 }

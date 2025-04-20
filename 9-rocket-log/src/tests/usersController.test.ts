4271eb6fce4149031cbed707ac390061ec869679
Danilo Carlos Marques <marquesdanilocarlos@gmail.com>
2025-04-20 13:52:59 -0300

    Aula 150 - Deleção do usuário de teste

--- 9-rocket-log/src/tests/usersController.test.ts
+++ 9-rocket-log/src/tests/usersController.test.ts
@@ -1,7 +1,17 @@
 import request from "supertest";
 import app from "@/app";
+import prisma from "@/database/prisma";
 
 describe("UsersController", () => {
+
+    let userId: string;
+
+    afterAll(async () => {
+        await prisma.user.delete({
+            where: {id: userId}
+        });
+    });
+
     it('should create a new user', async () => {
         const response = await request(app).post('/users').send({
             name: 'John Doe',
@@ -12,5 +22,7 @@ describe("UsersController", () => {
         expect(response.status).toBe(201);
         expect(response.body).toHaveProperty('id');
         expect(response.body.name).toBe('John Doe');
+
+        userId = response.body.id;
     });
 });
